9d4f67048708d7cb621ee58af0e8e9b7cbbdb73b
Matthew T. Adams <matthew@matthewadams.me>
2016-09-07 14:39:59 -0500

    ready for initial publish

---
+++ build.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'asciidoctor'
+require 'asciidoctor/cli/options'
+require 'asciidoctor/cli/invoker'
+require 'asciidoctor-diagram'
+require 'fileutils'
+require 'optparse'
+require 'pathname'
+
+out = "out"
+src = "reference"
+FileUtils.rm_rf(out)
+FileUtils.mkdir_p(out + "/stylesheets")
+FileUtils.cp_r(src + "/stylesheets",out)
+Dir.glob(src+"/**/*.adoc") {
+  |file|
+  dest = Pathname.new(out+"/"+file)
+  destDir = dest.dirname()
+  relDest = (Pathname.new(out)).relative_path_from(destDir).to_s
+  FileUtils.mkdir_p(destDir)
+  arr = [
+    "-D",destDir.to_s,
+    "-a","source-highlighter=prettify",
+    "-a","linkcss",
+    "-a","stylesheet=" + relDest + "/stylesheets/asciidoctor.css",
+    file ]
+  Asciidoctor::Cli::Invoker.new(*arr).invoke!
+}
